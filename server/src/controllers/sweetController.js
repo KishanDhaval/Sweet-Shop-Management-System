e54c32c20fab407cffb29783ddcf68cb077e2888
KishanDhaval <kmprajapati1910@gmail.com>
2025-07-16 15:34:34 +0530

    Implimented controllers

---
+++ server/src/controllers/sweetController.js
@@ -0,0 +1,68 @@
+const SweetShop = require('../models/sweetShop');
+
+const shop = new SweetShop();
+
+module.exports = {
+  addSweet: (req, res) => {
+    try {
+      const sweet = shop.addSweet(req.body);
+      res.status(201).json(sweet);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  },
+
+  viewSweets: (req, res) => {
+    res.json(shop.viewSweets());
+  },
+
+  deleteSweet: (req, res) => {
+    try {
+      const sweet = shop.deleteSweet(req.params.id);
+      res.json(sweet);
+    } catch (err) {
+      res.status(404).json({ error: err.message });
+    }
+  },
+
+  deleteSweetsByCategory: (req, res) => {
+    try {
+      const sweets = shop.deleteSweetsByCategory(req.params.category);
+      res.json(sweets);
+    } catch (err) {
+      res.status(404).json({ error: err.message });
+    }
+  },
+
+  searchByName: (req, res) => {
+    res.json(shop.searchByName(req.query.name || ''));
+  },
+
+  searchByCategory: (req, res) => {
+    res.json(shop.searchByCategory(req.query.category || ''));
+  },
+
+  searchByPriceRange: (req, res) => {
+    const min = Number(req.query.min) || 0;
+    const max = Number(req.query.max) || Number.MAX_SAFE_INTEGER;
+    res.json(shop.searchByPriceRange(min, max));
+  },
+
+  purchaseSweet: (req, res) => {
+    try {
+      const sweet = shop.purchaseSweet(req.params.id, Number(req.body.qty));
+      res.json(sweet);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  },
+
+  restockSweet: (req, res) => {
+    try {
+      const sweet = shop.restockSweet(req.params.id, Number(req.body.qty));
+      res.json(sweet);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+};
